5bd98f03a35cafb75811b125f63319773f4c6d0c
Dwi Ratnasari <dwiratnasari391@gmail.com>
2017-05-29 14:07:01 +0700

    Add files via upload

---
+++ pernyataanDoWhile.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Penyataan While</title>
+</head>
+<body>
+<?php
+$counter = 1;
+while($counter > 2)
+{
+echo "perulangan while ke-$counter";
+}
+$counter = 1;
+do
+{
+echo " <b>Perulangan Do While ke-$counter </b>";
+}
+while ($counter > 2);
+?>
+</body>
+</html>
